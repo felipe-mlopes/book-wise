8a15d1778d488fbde8bd0ddc7fe795b97d9e48d9
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-07-08 15:24:22 -0300

    refactor: update card component

---
+++ src/components/Feed/BookReviews/BookReviewCard.tsx
@@ -0,0 +1,69 @@
+import Image from 'next/image';
+
+import { handleTransformDateInDaysOrWeeks } from '@/utils/transform-dates';
+import { capitalizeFirstLetters } from '@/utils/capitalize-first-letters';
+
+import { StarRating } from '../../Global/StarRating';
+import { Avatar } from '../../Global/Avatar';
+
+interface CardTypes {
+  rate: number,
+  description: string,
+  createdDate: string,
+  userAvatar: string,
+  userName: string
+  bookAuthor: string,
+  bookCover: string,
+  bookName: string
+}
+
+export function BookReviewCard({ 
+  rate, 
+  description,
+  createdDate, 
+  userAvatar, 
+  userName, 
+  bookAuthor, 
+  bookCover, 
+  bookName 
+}: CardTypes) {
+
+  const cover = bookCover ? bookCover.slice(19).replace('jpg', 'png') : ''
+  const createdAt = handleTransformDateInDaysOrWeeks(createdDate)
+  const bookTitle = capitalizeFirstLetters(bookName)
+
+  return (
+    <div className="space-y-8 p-6 bg-gray700 rounded-lg border-solid border-2 border-transparent hover:border-solid hover:border-2 hover:border-gray600">
+      <div className="flex items-start justify-between">
+        <div className="flex items-center gap-4">
+          <Avatar src={userAvatar} width={40} height={40} />
+          <div>
+            <p className="text-gray100 text-base">{userName}</p>
+            <p className="text-gray400 text-sm">{createdAt}</p>
+          </div>
+        </div>
+        <StarRating fill={rate} />
+      </div>
+
+      <div className="flex gap-5">
+        <Image
+          src={cover}
+          alt="capa do livro"
+          width={108}
+          height={152}
+          className="rounded"
+        />
+        <div className="flex flex-col justify-between gap-5">
+          <div>
+            <strong className="text-gray100 text-base">{bookTitle}</strong>
+            <p className="text-gray400 text-sm">{bookAuthor}</p>
+          </div>
+          <p className="text-gray300 text-sm">
+            {description}
+            <a className="text-purple100 cursor-pointer">ver mais</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
