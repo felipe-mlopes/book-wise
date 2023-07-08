0f2c34d59a553967c9266012e0b1d73cde3a36dc
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-07-08 15:55:21 -0300

    chore: add new features

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,8 +29,29 @@ module.exports = {
         gray800: '#0E1116',
         red: '#F75A68'
       },
+      fill: {
+        white: '#FFFFFF',
+        black: '#000000',
+        green50: '#7FD1CC',
+        green100: '#50B2C0',
+        green200: '#255D6A',
+        green300: '#0A313C',
+        purple50: '#9694F5',
+        purple100: '#8381D9',
+        purple200: '#2A2879',
+        gray100: '#F8F9FC',
+        gray200: '#E6E8F2',
+        gray300: '#D1D6E4',
+        gray400: '#8D95AF',
+        gray500: '#303F73',
+        gray600: '#252D4A',
+        gray700: '#181C2A',
+        gray800: '#0E1116',
+        red: '#F75A68'
+      },
       boxShadow: {
-        modal: '4px 16px 24px rgba(0, 0, 0, 0.25)'
+        modal: '4px 16px 24px rgba(0, 0, 0, 0.25)',
+        sidebar: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)'
       },
       animation: {
         overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
