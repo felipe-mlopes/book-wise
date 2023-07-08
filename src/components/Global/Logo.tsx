58445986d8af45dee136608a26c05e32df50ca7c
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-07-08 15:40:18 -0300

    refactor: change img to icon and tags html

--- src/components/Global/Logo.tsx
+++ src/components/Global/Logo.tsx
@@ -1,22 +1,19 @@
-import Image from 'next/image'
-import logo from '../../assets/icons/logo.svg'
-
 interface LogoProps {
   w: number;
   h: number;
-  size: string;
+  size: string
 }
 
 export function Logo({ w, h, size }: LogoProps) {
   return (
     <div className="flex items-center gap-3">
-      <Image src={logo} alt="" width={w} height={h} />
-      <h2
-        className="bg-clip-text text-transparent bg-gradient-to-r from-green50 to-purple50 font-bold"
-        style={{ fontSize: `${size}` }}
-      >
+      <svg width={w} height={h} viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M38 46.6L36.8 45.6C32.8 41.8 30 39.4 30 36.4C30 34 32 32 34.4 32C35.8 32 37.2 32.6 38 33.6C38.8 32.6 40.2 32 41.6 32C44 32 46 33.8 46 36.4C46 39.4 43.2 41.8 39.2 45.6L38 46.6Z" fill="#50B2C0"/>
+        <path d="M36 4C38.2 4 40 5.8 40 8V26.16L38 26L36 26.16V8H26V24L21 19.5L16 24V8H12V40H26.16C26.4 41.44 26.9 42.78 27.6 44H12C9.8 44 8 42.2 8 40V8C8 5.8 9.8 4 12 4H36Z" fill="#8381D9"/>
+      </svg>
+      <h1 className={`bg-clip-text text-transparent bg-gradient-to-r from-green50 to-purple50 font-bold text-[${size}]`}>
         BookWise
-      </h2>
+      </h1>
     </div>
   )
 }
