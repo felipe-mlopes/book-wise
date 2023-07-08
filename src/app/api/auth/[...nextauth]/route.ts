0a08cb3ee90d87753b06e90d30d2ad66bbe5fd57
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-07-08 15:45:55 -0300

    feat: update auth rout

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,44 +1,73 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from 'next-auth/providers/google'
-import GitHubProvider from "next-auth/providers/github"
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
+import GitHubProvider, { GithubProfile } from "next-auth/providers/github"
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from '@/lib/prisma';
 import { VerificationToken } from "@prisma/client";
 import { Adapter } from "next-auth/adapters";
-
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter<VerificationToken>,
 
   session: {
     strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60 // 30 days
+    maxAge: 30 * 24 * 60 * 60, // 30 days
   },
 
   secret: process.env.NEXTAUTH_SECRET,
 
   providers: [
     GoogleProvider({
+      id: 'google',
+      name: 'google',
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+          scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid'
+        }
+      },
+      profile(profile: GoogleProfile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          avatar_url: profile.picture,
+          image: profile.picture,
+          email: profile.email,
+        }
+      },
     }),
 
     GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID ?? '',
-      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',
+      id: 'github',
+      name: 'github',
+      clientId: process.env.GITHUB_ID ?? '',
+      clientSecret: process.env.GITHUB_SECRET ?? '',
+      profile(profile: GithubProfile) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name || profile.login,
+          avatar_url: profile.avatar_url,
+          image: profile.avatar_url,
+          email: profile.email ?? '',
+        }
+      },
     })
   ],
 
   callbacks: {
-    async session({ session, user }) {
+    async session({ session, token }) {
       return {
         ...session,
-        user
+        token
       }
     }
-  }
+  },
+  
 }
-
 
 const handler = NextAuth(authOptions)
 
