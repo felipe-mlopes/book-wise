ec74f02d279b4c8f739960b8d0f98f0feba57673
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-07-09 21:46:04 -0300

    refactor: update search input component

--- src/components/Global/SearchInput.tsx
+++ src/components/Global/SearchInput.tsx
@@ -6,36 +6,31 @@ import { SearchIcon } from '../Icons/SearchIcon';
 
 interface SearchInputProps {
   placeholder: string;
-  type: 'explore' | 'profile'
 }
 
-export function SearchInput({ placeholder, type }: SearchInputProps) {
+export function SearchInput({ placeholder }: SearchInputProps) {
   const { setSearch } = useFilter()
 
   function handleSearchToTitleOrAuthor(e: ChangeEvent<HTMLInputElement>) {
-    if (type === 'explore') {
-      const query = e.currentTarget.value
+    const query = e.currentTarget.value
       setSearch(query)
-    }
   }
   
   return (
-    <form>
-      <label
-        htmlFor="searchInput"
-        className="flex items-center justify-between gap-2 py-3.5 px-5 border border-solid border-gray500 rounded focus-within:border-green200 group"
-      >
-        <input
-          type="text"
-          name="searchInput"
-          placeholder={placeholder}
-          onChange={handleSearchToTitleOrAuthor}
-          className="min-w-[23rem] text-sm text-gray200 placeholder:text-gray400 placeholder:text-sm cursor-text"
-        />
-        <button type="submit" className=''>
-          <SearchIcon styles="cursor-pointer group-focus-within:text-green200" />
-        </button>
-      </label>
-    </form>
+    <label
+      htmlFor="searchInput"
+      className="flex items-center justify-between gap-2 py-3.5 px-5 border border-solid border-gray500 rounded focus-within:border-green200 group"
+    >
+      <input
+        type="text"
+        name="searchInput"
+        placeholder={placeholder}
+        onChange={handleSearchToTitleOrAuthor}
+        className="min-w-[23rem] text-sm text-gray200 placeholder:text-gray400 placeholder:text-sm cursor-text"
+      />
+      <button type="button">
+        <SearchIcon styles="cursor-pointer group-focus-within:text-green200" />
+      </button>
+    </label>
   );
 }
