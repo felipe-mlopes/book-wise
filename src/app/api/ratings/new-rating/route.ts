2dcaaf440af95e52a2bac3b6a617e2345be99318
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-07-15 20:26:25 -0300

    fix: delete user and book body requests

--- src/app/api/ratings/new-rating/route.ts
+++ src/app/api/ratings/new-rating/route.ts
@@ -2,14 +2,12 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  const { user_id, user, book_id, book, description, rate } = await request.json()
+  const { user_id, book_id, description, rate } = await request.json()
 
   const newRating = await prisma.rating.create({
     data: {
       user_id,
-      user,
       book_id,
-      book,
       description,
       rate
     }
