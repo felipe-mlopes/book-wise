d2cd9fb254e66c470766e6b0b9278325b0b451b6
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-07-15 12:16:49 -0300

    feat: separate user rating from other ratings

--- src/app/api/books/book/[id]/route.ts
+++ src/app/api/books/book/[id]/route.ts
@@ -1,40 +1,38 @@
 import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
+  const token = await getToken({
+    req: request,
+    secret: process?.env?.NEXTAUTH_SECRET,
+    cookieName: "next-auth.session-token"
+  })
+
+  const userId = token?.sub
+
   const pathname = request.nextUrl.pathname
   const idStartIndex = pathname.indexOf('=') + 1
   const bookId = pathname.substring(idStartIndex)
-
+  
   const bookRatings = await prisma.book.findUnique({
     where: {
       id: bookId
     },
     select: {
-      id: true,
-      name: true,
-      author: true,
-      cover_url: true,
-      categories: {
-        select: {
-          category: {
-            select: {
-              name: true
-            }
-          }
-        }
-      },
-      total_pages: true,
       ratings: {
         orderBy: {
           created_at: 'desc'
         },
         select: {
+          id: true,
           created_at: true,
           rate: true,
           description: true,
           user: {
             select: {
+              id: true,
               name: true,
               avatar_url: true
             }
@@ -44,5 +42,9 @@ export async function GET(request: NextRequest) {
     }
   })
 
-  return NextResponse.json({ bookRatings })
+  const ratings = bookRatings?.ratings.filter(rating => rating.user.id !== userId)
+
+  const userRatings = bookRatings?.ratings.filter(rating => rating.user.id === userId)
+
+  return NextResponse.json({ ratings, userRatings })
 }
