500b6f35fb305e90fd3311be0b6a2db414b489ad
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-07-15 14:30:57 -0300

    style: update using data-attributes

--- src/components/Icons/ChevronIcon.tsx
+++ src/components/Icons/ChevronIcon.tsx
@@ -1,14 +1,27 @@
 interface ChevronIconProps {
   type: 'right' | 'left'
-  w: number,
-  h: number
   isDisabled?: boolean
+  isTag?: boolean
 }
 
-export function ChevronIcon({ type, w, h, isDisabled}: ChevronIconProps) {
+export function ChevronIcon({ type, isDisabled = false, isTag = false}: ChevronIconProps) {
   return (
-    <svg width={w} height={h} viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" className={`${type === 'left' && 'rotate-180'}`}>
-      <path fillRule="evenodd" clipRule="evenodd" d="M5.64645 2.64645C5.84171 2.45118 6.15829 2.45118 6.35355 2.64645L11.3536 7.64645C11.5488 7.84171 11.5488 8.15829 11.3536 8.35355L6.35355 13.3536C6.15829 13.5488 5.84171 13.5488 5.64645 13.3536C5.45118 13.1583 5.45118 12.8417 5.64645 12.6464L10.2929 8L5.64645 3.35355C5.45118 3.15829 5.45118 2.84171 5.64645 2.64645Z" className={`${isDisabled === true ? 'fill-gray700' : 'fill-purple100'}`}/>
+    <span role="img" aria-label="arrow-icon">
+      <svg 
+        viewBox="0 0 16 16" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg" 
+        data-tag={isTag}
+        className={`w-4 h-4 data-[tag=true]:w-12 data-[tag=true]:h-12 ${type === 'left' && 'rotate-180'} `}
+      >
+          <path 
+            fillRule="evenodd" 
+            clipRule="evenodd" 
+            d="M5.64645 2.64645C5.84171 2.45118 6.15829 2.45118 6.35355 2.64645L11.3536 7.64645C11.5488 7.84171 11.5488 8.15829 11.3536 8.35355L6.35355 13.3536C6.15829 13.5488 5.84171 13.5488 5.64645 13.3536C5.45118 13.1583 5.45118 12.8417 5.64645 12.6464L10.2929 8L5.64645 3.35355C5.45118 3.15829 5.45118 2.84171 5.64645 2.64645Z" 
+            data-disable={isDisabled}
+            className="fill-purple100 data-[disable=true]:fill-gray700"
+          />
       </svg>
+    </span>
   )
 }
--- src/components/Icons/CloseIcon.tsx
+++ src/components/Icons/CloseIcon.tsx
@@ -1,12 +1,35 @@
 import { SVGAttributes } from "react";
 
-interface CloseIconProps extends SVGAttributes<SVGSVGElement> {}
+interface CloseIconProps extends SVGAttributes<SVGSVGElement> {
+  isCancelButton?: boolean
+}
 
-export function CloseIcon({...props}: CloseIconProps) {
+export function CloseIcon({isCancelButton = false, ...props}: CloseIconProps) {
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <path fillRule="evenodd" clipRule="evenodd" d="M19.2803 4.71967C19.5732 5.01256 19.5732 5.48744 19.2803 5.78033L5.78033 19.2803C5.48744 19.5732 5.01256 19.5732 4.71967 19.2803C4.42678 18.9874 4.42678 18.5126 4.71967 18.2197L18.2197 4.71967C18.5126 4.42678 18.9874 4.42678 19.2803 4.71967Z" fill="#8D95AF"/>
-      <path fillRule="evenodd" clipRule="evenodd" d="M4.71967 4.71967C5.01256 4.42678 5.48744 4.42678 5.78033 4.71967L19.2803 18.2197C19.5732 18.5126 19.5732 18.9874 19.2803 19.2803C18.9874 19.5732 18.5126 19.5732 18.2197 19.2803L4.71967 5.78033C4.42678 5.48744 4.42678 5.01256 4.71967 4.71967Z" fill="#8D95AF"/>
-    </svg>
+    <span role="img" aria-label="close-icon">
+      <svg 
+        width="24" 
+        height="24" 
+        viewBox="0 0 24 24" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg" 
+        {...props}
+      >
+        <path 
+          fillRule="evenodd" 
+          clipRule="evenodd" 
+          d="M19.2803 4.71967C19.5732 5.01256 19.5732 5.48744 19.2803 5.78033L5.78033 19.2803C5.48744 19.5732 5.01256 19.5732 4.71967 19.2803C4.42678 18.9874 4.42678 18.5126 4.71967 18.2197L18.2197 4.71967C18.5126 4.42678 18.9874 4.42678 19.2803 4.71967Z" 
+          data-active={isCancelButton}
+          className="fill-gray400 data-[active=true]:fill-purple100"
+        />
+        <path 
+          fillRule="evenodd" 
+          clipRule="evenodd" 
+          d="M4.71967 4.71967C5.01256 4.42678 5.48744 4.42678 5.78033 4.71967L19.2803 18.2197C19.5732 18.5126 19.5732 18.9874 19.2803 19.2803C18.9874 19.5732 18.5126 19.5732 18.2197 19.2803L4.71967 5.78033C4.42678 5.48744 4.42678 5.01256 4.71967 4.71967Z" 
+          data-active={isCancelButton}
+          className="fill-gray400 data-[active=true]:fill-purple100"
+        />
+      </svg>
+    </span>
   )
 }
