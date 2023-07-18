54920594444c738d878792f886817e0f0147ce07
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-07-17 21:19:05 -0300

    feat: create hook useAddUseRating

--- src/hooks/use-book-ratings.ts
+++ src/hooks/use-book-ratings.ts
@@ -1,5 +1,6 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { BookRatingsProps } from "@/@types/book-ratings";
+import { CommentProps } from "@/components/Global/BookCard/BookModal/Comment";
 
  interface BookRatingsFetchResponse {
   ratings: BookRatingsProps[],
@@ -11,15 +12,33 @@ const fetcher = (id: string): Promise<BookRatingsFetchResponse> => fetch(
     method: 'GET' 
   }).then(res => res.json())
 
+const newUserRating = (data: CommentProps) => {
+  return fetch('api/ratings/new-rating', {
+    method: 'POST',
+    body: JSON.stringify(data)
+  })
+}
+
 export function useBookRatings(id: string) {
   const { data, isLoading } = useQuery({
     queryFn: () => fetcher(id),
-    queryKey: ['ratings', 'userRatings', id]
+    queryKey: ['ratings', 'userRatings', id],
   })
-  
+
   return {
     ratingsData: data?.ratings,
     userRatingData: data?.userRatings,
     isLoading
   }
+}
+
+export function useAddUserRating() {
+  const queryClient = useQueryClient()
+
+  return useMutation({ 
+      mutationFn: newUserRating,
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ['userRatings'] })
+      }
+    })
 }
