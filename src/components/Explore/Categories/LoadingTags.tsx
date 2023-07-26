3c8518912058985710a5327122cfae93468954ef
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-07-26 15:59:57 -0300

    refactor: change skeleton to animate-pulse

--- src/components/Explore/Categories/LoadingTags.tsx
+++ src/components/Explore/Categories/LoadingTags.tsx
@@ -1,15 +1,15 @@
-import { Skeleton } from "@/components/Global/Skeleton";
 import { ChevronIcon } from "@/components/Icons/ChevronIcon";
 
-const fakeList = [...Array(8).keys()]
 
 export function LoadingTags() {
+  const fakeList = [...Array(8)]
+
   return (
     <ul className="flex items-center gap-3">
       <ChevronIcon type="left" isDisabled={true} isTag={true} />
       {fakeList.map((_, idx) => (
-        <li key={idx}>
-          <Skeleton className="w-[5.5rem] h-8 rounded-full" />
+        <li key={idx} className="animate-pulse">
+          <div className="w-[5.5rem] h-8 bg-gray700 rounded-full" />
         </li>
       ))}
       <ChevronIcon type="right" isDisabled={false} isTag={true} />
