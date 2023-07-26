5e102a96f0a15aa3953b79c629a2d66a68953213
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-07-26 16:32:33 -0300

    fix: add bookId as param in useAddUserRating hook

--- src/components/Global/BookCard/BookModal/Comment.tsx
+++ src/components/Global/BookCard/BookModal/Comment.tsx
@@ -10,6 +10,7 @@ import { CloseIcon } from "@/components/Icons/CloseIcon";
 import { CheckIcon } from "@/components/Icons/CheckIcon";
 import { useAddUserRating, useBookRatings } from "@/hooks/use-book-ratings";
 import { useQueryClient } from "@tanstack/react-query";
+import { LoadingReviews } from "./LoadingReviews";
 
 interface CommentTypes {
   bookId: string,
@@ -35,12 +36,12 @@ const commentSchema = z.object({
 })
 
 export function Comment({ bookId, userId, userName, userAvatarUrl }: CommentTypes) {
-  const { 
-    register, 
+  const {
+    register,
     setValue,
-    handleSubmit, 
+    handleSubmit,
     watch,
-    formState: { isSubmitting, isSubmitSuccessful ,errors } 
+    formState: { isSubmitting, isSubmitSuccessful, errors }
   } = useForm<CommentProps>({
     resolver: zodResolver(commentSchema)
   })
@@ -48,7 +49,7 @@ export function Comment({ bookId, userId, userName, userAvatarUrl }: CommentType
   const watchComment = watch("description")
   const characterTextAreaCount = watchComment ? watchComment.length : 0
 
-  const { mutate: onCreateComment } = useAddUserRating()
+  const { mutate: onCreateComment } = useAddUserRating(bookId)
 
   function handleSetRate(rate: number) {
     setValue('rate', rate)
@@ -65,22 +66,26 @@ export function Comment({ bookId, userId, userName, userAvatarUrl }: CommentType
     onCreateComment(commentData)
   }
 
+  if (isSubmitting) {
+    return <LoadingReviews amountReviews={1} />
+  }
+
   if (isSubmitSuccessful) {
     return null
   }
 
   return (
-    <form 
+    <form
       onSubmit={handleSubmit(handleCreateComment)}
       className="p-6 space-y-6 bg-gray700 rounded-lg"
     >
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-4">
-          <Avatar src={userAvatarUrl} width={40} height={40}/>
+          <Avatar src={userAvatarUrl} width={40} height={40} />
           <strong className="text-gray100">{userName}</strong>
         </div>
-        <Rating 
-          emptyStyle={{ display: "flex" }} 
+        <Rating
+          emptyStyle={{ display: "flex" }}
           fillStyle={{ display: "-webkit-inline-box" }}
           size={28}
           SVGstorkeWidth={1}
@@ -92,28 +97,28 @@ export function Comment({ bookId, userId, userName, userAvatarUrl }: CommentType
       </div>
       <div className="flex flex-col items-end gap-3 max-w-[516px]">
         <label htmlFor="comment" className="relative flex flex-col w-full group">
-          <textarea 
-            id="comment" 
-            placeholder="Escreva sua avaliação" 
-            maxLength={450} 
+          <textarea
+            id="comment"
+            placeholder="Escreva sua avaliação"
+            maxLength={450}
             className="bg-gray800 py-[14px] min-h-[164px] px-5 border border-solid border-gray500 rounded text-sm text-gray200 placeholder:text-gray400 placeholder:text-sm cursor-text resize-none outline-none scrollbar-thin scrollbar-track-gray700 scrollbar-thumb-gray600 group-focus-within:border-green200"
             {...register('description')}
           />
           <span className="absolute bottom-2 right-3 text-gray400 text-xs group-focus-within:text-green200">{characterTextAreaCount}/450</span>
         </label>
         <span className="space-y-1 w-full pl-1">
-          {errors.description && <p className="text-xs text-red">{errors.description.message}</p>}
-          {errors.rate && <p className="text-xs text-red">{errors.rate.message}</p>}
+          {errors.description && <p className="text-xs text-red100">{errors.description.message}</p>}
+          {errors.rate && <p className="text-xs text-red100">{errors.rate.message}</p>}
         </span>
         <div className="flex items-center gap-2">
-          <button 
+          <button
             type="reset"
-            disabled={isSubmitting} 
+            disabled={isSubmitting}
             className="flex items-center justify-center bg-gray600 rounded p-2 cursor-pointer hover:bg-gray500"
           >
             <CloseIcon isCancelButton={true} />
           </button>
-          <button 
+          <button
             type="submit"
             disabled={isSubmitting}
             className="flex items-center justify-center bg-gray600 rounded p-2 cursor-pointer hover:bg-gray500"
