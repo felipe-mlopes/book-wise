846f6d12c535786c5937784511a70f86ff76d7a9
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-07-26 16:23:11 -0300

    refactor: change nav tag to aside tag

--- src/components/Global/Sidebar/index.tsx
+++ src/components/Global/Sidebar/index.tsx
@@ -5,14 +5,12 @@ import { Menu } from "./Menu";
 
 export function Sidebar() {
   return (
-    <nav className="row-span-3">
-      <div className="flex flex-col items-center justify-between px-[3.25rem] pt-10 pb-6 rounded-xl h-screen bg-gray700">
-        <div className="flex flex-col gap-16">
-          <Logo w={24} h={24} size="1.25rem" />
-          <Menu />
-        </div>
-        <Login />
+    <aside className="row-span-3 flex flex-col items-center justify-between pt-10 pb-6 rounded-xl h-screen bg-gray700">
+      <div className="flex flex-col gap-16">
+        <Logo w={24} h={24} size="1.25rem" />
+        <Menu />
       </div>
-    </nav>
+      <Login />
+    </aside>
   );
 }
