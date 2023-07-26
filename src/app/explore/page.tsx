8f0a66b1f572cd4d0145767d3ca98b85e462dba3
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-07-26 15:47:40 -0300

    feat: change component Tags to Categories

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,17 +1,17 @@
 import { Sidebar } from "@/components/Global/Sidebar";
 import { Header } from "@/components/Global/Header";
-import { Tags }from "@/components/Explore/Categories/Tags";
-import { BookList } from "@/components/Explore/BooksList/BookList";
+import { Categories } from "@/components/Explore/Categories";
+import { BooksList } from "@/components/Explore/BooksList";
 
 
 export default function Explore() {
   return (
-    <div className="grid grid-rows-feed grid-cols-feed mt-5 ml-5 gap-x-16 gap-y-10 overflow-hidden">
+    <div className="grid grid-rows-feed grid-cols-feed mt-5 mb-4 ml-5 gap-x-16 gap-y-10 overflow-hidden">
       <Sidebar />
       <Header page="explore" />
-      <main className="row-span-2 col-span-2 space-y-12 ml-8 mt-10">
-        <Tags />
-        <BookList />
+      <main className="row-span-2 col-span-2 space-y-12 ml-8 mt-10 flex-1">
+        <Categories />
+        <BooksList />
       </main>
     </div>
   )
