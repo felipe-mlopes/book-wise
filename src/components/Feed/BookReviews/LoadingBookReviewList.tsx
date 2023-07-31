9fcc903309338a8b13984a8063d46c6803e5182f
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-07-31 06:53:48 -0300

    refactor: update to mobile

--- src/components/Feed/BookReviews/LoadingBookReviewList.tsx
+++ src/components/Feed/BookReviews/LoadingBookReviewList.tsx
@@ -1,42 +1,78 @@
 export function LoadingBookReviewList() {
-  const fakeList = [...Array(3)]
+  const fakeList = [...Array(2)]
 
   return (
     <div className="animate-pulse flex flex-col gap-3">
       {fakeList.map((_, idx) => (
-        <div key={idx} className="space-y-8 p-6  bg-gray700 rounded-lg">
-          <div className="flex items-start justify-between">
-            <div className="flex items-center gap-4">
+        <div key={idx}>
+          <div className="hidden invisible m:block m:visible m:space-y-8 m:p-6 m:bg-gray700 m:rounded-lg">
+            <div className="flex items-start justify-between">
+              <div className="flex items-center gap-4">
+                <div
+                  role="img"
+                  aria-label="avatar do usuário"
+                  className="w-12 h-12 bg-gray600 rounded-full"
+                />
+                <div className="flex flex-col gap-1">
+                  <p className="w-28 h-6 bg-gray600 rounded" />
+                  <p className="w-28 h-[1.3rem] bg-gray600 rounded" />
+                </div>
+              </div>
+              <span
+                role="img"
+                aria-label="ícones de estrelas"
+                className="w-24 h-4 bg-gray600 rounded"
+              />
+            </div>
+
+            <div className="flex gap-5">
               <div
                 role="img"
-                aria-label="avatar do usuário"
-                className="w-12 h-12 bg-gray600 rounded-full"
+                aria-label="capa do livro comentado"
+                className="w-[6.75rem] h-auto bg-gray600 rounded"
               />
-              <div className="flex flex-col gap-1">
-                <p className="w-28 h-6 bg-gray600 rounded" />
-                <p className="w-28 h-[1.3rem] bg-gray600 rounded" />
+              <div className="flex flex-col justify-between gap-5">
+                <div className="flex flex-col gap-1">
+                  <strong className="w-36 h-6 bg-gray600 rounded" />
+                  <p className="w-28 h-6 bg-gray600 rounded" />
+                </div>
+                <p className="lg:w-60 xl:w-[27rem] h-[5.5rem] bg-gray600 rounded" />
               </div>
             </div>
-            <span
-              role="img"
-              aria-label="ícones de estrelas"
-              className="w-24 h-4 bg-gray600 rounded"
-            />
           </div>
 
-          <div className="flex gap-5">
-            <div
-              role="img"
-              aria-label="capa do livro comentado"
-              className="w-[6.75rem] h-auto bg-gray600 rounded"
-            />
-            <div className="flex flex-col justify-between gap-5">
-              <div className="flex flex-col gap-1">
-                <strong className="w-36 h-6 bg-gray600 rounded" />
-                <p className="w-28 h-6 bg-gray600 rounded" />
+          <div className="space-y-6 bg-gray700 rounded-lg p-4 m:hidden m:invisible">
+            <div className="flex items-start justify-between">
+              <div className="flex flex-col items-start gap-3">
+                <div
+                  role="img"
+                  aria-label="avatar do usuário"
+                  className="w-12 h-12 bg-gray600 rounded-full"
+                />
+                <div className="flex flex-col gap-1">
+                  <p className="w-28 h-4 bg-gray600 rounded" />
+                  <p className="w-28 h-3 bg-gray600 rounded" />
+                </div>
               </div>
-              <p className="w-[27rem] h-[5.5rem] bg-gray600 rounded" />
+              <span
+                role="img"
+                aria-label="ícones de estrelas"
+                className="w-24 h-4 bg-gray600 rounded"
+              />
             </div>
+
+            <div className="flex gap-5">
+              <div
+                role="img"
+                aria-label="capa do livro comentado"
+                className="w-[5.5rem] h-[8.25rem] bg-gray600 rounded"
+              />
+              <div className="flex flex-col justify-between">
+                <strong className="w-36 h-5 bg-gray600 rounded" />
+                <p className="w-28 h-4 bg-gray600 rounded" />
+              </div>
+            </div>
+            <p className="w-auto h-8 bg-gray600 rounded" />
           </div>
         </div>
       ))}
--- src/components/Feed/BookReviews/LoadingLastReviewUserLogged.tsx
+++ src/components/Feed/BookReviews/LoadingLastReviewUserLogged.tsx
@@ -1,30 +1,56 @@
 export function LoadingLastReviewUserLogged() {
   return (
-    <div className="animate-pulse flex gap-6 bg-gray600 rounded-lg py-5 px-6">
-      <div
-        role="img"
-        aria-label="capa do livro"
-        className="w-[6.75rem] h-[9.5rem] bg-gray500 rounded"
-      />
+    <>
+      <div className="animate-pulse hidden invisible m:flex m:gap-6 m:visible m:bg-gray600 m:rounded-lg m:py-5 m:px-6">
+        <div
+          role="img"
+          aria-label="capa do livro"
+          className="w-[6.75rem] h-[9.5rem] bg-gray500 rounded"
+        />
 
-      <div className="space-y-6 w-[26.75rem] ">
-        <div className="space-y-3">
-          <div className="flex items-center justify-between">
-            <p className="w-10 h-4 bg-gray500 rounded" />
-            <span
-              role="img"
-              aria-label="ícones de estrela sobre avaliação dos livros"
-              className="w-24 h-4 bg-gray500 rounded"
-            />
+        <div className="space-y-6">
+          <div className="space-y-3">
+            <div className="flex items-center justify-between">
+              <p className="w-10 h-4 bg-gray500 rounded" />
+              <span
+                role="img"
+                aria-label="ícones de estrela sobre avaliação dos livros"
+                className="w-24 h-4 bg-gray500 rounded"
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+              <strong className="w-36 h-6 bg-gray500 rounded" />
+              <p className="w-28 h-6 bg-gray500 rounded" />
+            </div>
           </div>
-          <div className="flex flex-col gap-1">
-            <strong className="w-36 h-6 bg-gray500 rounded" />
-            <p className="w-28 h-6 bg-gray500 rounded" />
+
+          <p className="lg:w-60 xl:w-[27rem] h-14 bg-gray500 rounded" />
+        </div>
+      </div>
+
+      <div className="animate-pulse flex flex-col gap-6 bg-gray600 rounded-lg p-4 m:hidden m:invisible">
+        <div className="flex items-center justify-between">
+          <p className="w-10 h-4 bg-gray500 rounded" />
+          <span
+            role="img"
+            aria-label="ícones de estrela sobre avaliação dos livros"
+            className="w-24 h-4 bg-gray500 rounded"
+          />
+        </div>
+        <div className="flex gap-4">
+          <div
+            role="img"
+            aria-label="capa do livro"
+            className="w-[5.5rem] h-[8.25rem] bg-gray500 rounded"
+          />
+          <div className="flex flex-col justify-between">
+            <strong className="w-36 h-4 bg-gray500 rounded" />
+            <p className="w-28 h-4 bg-gray500 rounded" />
           </div>
         </div>
 
-        <p className="w-[26.75rem] h-14 bg-gray500 rounded" />
+        <p className="w-60 h-8 bg-gray500 rounded" />
       </div>
-    </div>
+    </>
   )
 }
