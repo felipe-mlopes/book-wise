5b1ed5a7e906ed5976f7e6edd4190fc4aa2c08d1
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-07-31 22:27:27 -0300

    style: update to mobile

--- src/components/Global/Login/LoginModal.tsx
+++ src/components/Global/Login/LoginModal.tsx
@@ -12,18 +12,18 @@ export function LoginModal({ isOpen, onClose }: LoginModalProps) {
   }
 
   return (
-    <div 
-      className="bg-black/60 data-[state=open]:animate-overlayShow fixed inset-0" 
+    <div
+      className="bg-black/60 data-[state=open]:animate-overlayShow fixed inset-0"
       onClick={onClose}
     >
-      <div className="data-[state=open]:animate-contentShow fixed top-1/2 left-1/2 max-w-[516px] -translate-x-1/2 -translate-y-1/2 rounded-xl bg-gray700 px-[4.5rem] py-14 shadow-modal focus:outline-none" onClick={(e) => e.stopPropagation()}>
-        <div 
-          className="flex items-center justify-center p-3 absolute top-0 right-0 cursor-pointer" onClick={onClose}
+      <div className="data-[state=open]:animate-contentShow fixed top-1/2 left-1/2 max-w-[516px] -translate-x-1/2 -translate-y-1/2 rounded-xl bg-gray700 px-10 py-8 xs:px-14 xs:py-10 md:px-[4.5rem] md:py-14 shadow-modal focus:outline-none" onClick={(e) => e.stopPropagation()}>
+        <div
+          className="flex items-center justify-center p-3 absolute top-1 right-1 cursor-pointer" onClick={onClose}
         >
           <CloseIcon />
         </div>
-        <div className="flex flex-col items-center gap-10">
-          <h2 className="text-gray200 text-base font-bold">
+        <div className="flex flex-col items-center gap-6 xs:gap-8 md:gap-10">
+          <h2 className="text-gray200 text-base font-bold text-center mt-4 xs:mt-0">
             Faça login para deixar sua avaliação
           </h2>
           <LoginProviders />
--- src/components/Global/Login/LoginProviders.tsx
+++ src/components/Global/Login/LoginProviders.tsx
@@ -11,18 +11,18 @@ export function LoginProviders() {
     <div className="space-y-4">
       <button
         onClick={() => signIn('google')}
-        className="flex items-center gap-4 lg:gap-5 px-12 py-4 lg:py-5 lg:pl-6 lg:pr-28 bg-gray600 rounded-md cursor-pointer hover:bg-gray500"
+        className="flex items-center gap-4 lg:gap-5 px-5 xs:px-6 md:px-8 py-4 lg:py-5 lg:pl-6 lg:pr-28 bg-gray600 rounded-md cursor-pointer hover:bg-gray500"
       >
         <GoogleIcon />
-        <strong className="text-sm text-gray200 mr-4 lg:text-lg">Entrar com Google</strong>
+        <strong className="text-sm text-gray200 whitespace-nowrap lg:mr-4 lg:text-lg">Entrar com Google</strong>
       </button>
 
       <button
         onClick={() => signIn('github')}
-        className="flex items-center gap-4 lg:gap-5 px-12 py-4 lg:py-5 lg:pl-6 lg:pr-28 bg-gray600 rounded-md cursor-pointer hover:bg-gray500"
+        className="flex items-center gap-4 lg:gap-5 px-5 xs:px-6 md:px-8 py-4 lg:py-5 lg:pl-6 lg:pr-28 bg-gray600 rounded-md cursor-pointer hover:bg-gray500"
       >
         <GithubIcon />
-        <strong className="text-sm text-gray200 mr-4 lg:text-lg">Entrar com GitHub</strong>
+        <strong className="text-sm text-gray200 whitespace-nowrap lg:mr-4 lg:text-lg">Entrar com GitHub</strong>
       </button>
     </div>
   )
