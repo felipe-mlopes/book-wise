41fc82d77b76f6eb07d2f0445d56129f09cfa1b6
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-08-14 07:37:47 -0300

    refactor: change styles + remove BookReviews comp.

--- src/components/Global/BookCard/BookModal/index.tsx
+++ src/components/Global/BookCard/BookModal/index.tsx
@@ -1,20 +1,17 @@
 'use client'
 
 import { ReactNode } from "react";
-import { BookReviews } from "./BookReviews";
 import { CloseIcon } from "@/components/Icons/CloseIcon";
 
 interface BookModalProps {
   isOpen: boolean,
   onClose: () => void,
-  bookId: string,
   children: ReactNode
 }
 
 export function BookModal({
   isOpen,
   onClose,
-  bookId,
   children
 }: BookModalProps) {
 
@@ -24,22 +21,23 @@ export function BookModal({
 
   return (
     <div
-      className="bg-gray800/60 data-[state=open]:animate-overlayShow fixed inset-0"
+      role="dialog"
+      aria-label="bookmodal"
+      className="bg-gray800/60 data-[state=open]:animate-overlayShow fixed inset-0 z-50"
       onClick={onClose}
     >
       <div
-        className="data-[state=open]:animate-contentShow fixed inset-y-0 right-0 max-w-[660px] rounded-xl bg-gray800 px-12 py-14 shadow-modal overflow-y-auto scrollbar-thin scrollbar-track-gray700 scrollbar-thumb-gray600 focus:outline-none"
+        className="data-[state=open]:animate-contentShow fixed inset-y-0 right-0 max-w-[660px] rounded-xl bg-gray800 px-6 sm:px-12 py-14 shadow-modal overflow-y-auto scrollbar-thin scrollbar-track-gray700 scrollbar-thumb-gray600 focus:outline-none"
         onClick={(e) => e.stopPropagation()}
       >
         <div
-          className="flex items-center justify-center p-1 absolute top-3 right-12 cursor-pointer"
+          className="flex items-center justify-center p-1 absolute top-3 right-6 sm:right-12 cursor-pointer"
           onClick={onClose}
         >
           <CloseIcon />
         </div>
         <div className="space-y-10">
           {children}
-          <BookReviews bookId={bookId} />
         </div>
       </div>
     </div>
