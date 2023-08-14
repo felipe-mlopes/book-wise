9eabdfe5f795b6f8238239141d671902726176c9
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-08-14 07:25:35 -0300

    refactor: change sever side to client side

--- src/components/Explore/BooksList/index.tsx
+++ src/components/Explore/BooksList/index.tsx
@@ -1,13 +1,34 @@
+'use client'
+
+import { BookCardProps } from "@/@types/book-card"
+import { BookCard } from "@/components/Global/BookCard"
 import { useGetAllBooks } from "@/hooks/use-get-all-books"
-import { Books } from "./Books"
+import { LoadingBookList } from "./LoadingBookList"
 
 
-export async function BooksList() {
-  const { books } = await useGetAllBooks()
+export function BooksList() {
+  const { filteredBooks, isLoading } = useGetAllBooks()
+
+  if (isLoading) {
+    return <LoadingBookList />
+  }
 
   return (
-    <section className="flex flex-wrap gap-5">
-      <Books books={books} />
+    <section className="flex flex-wrap justify-center lg:justify-start gap-5">
+      {filteredBooks?.map((book: BookCardProps) => (
+        <BookCard
+          key={book.id}
+          id={book.id}
+          name={book.name}
+          coverUrl={book.coverUrl}
+          author={book.author}
+          categories={book.allCategories}
+          totalPages={book.totalPages}
+          ratingsAverage={book.ratingsAverage}
+          ratingsAmount={book.ratingsAmount}
+        />
+      ))
+      }
     </section>
   )
 }
--- src/components/Explore/Categories/index.tsx
+++ src/components/Explore/Categories/index.tsx
@@ -1,13 +1,47 @@
+'use client'
+
+import { Swiper, SwiperSlide } from "swiper/react"
 import { useGetCategories } from "@/hooks/use-get-categories"
-import { TagsList } from "./TagsList"
+import { useFilter } from "@/hooks/use-filter"
+import { LoadingTags } from "./LoadingTags"
 
 
-export async function Categories() {
-  const { categories } = await useGetCategories()
+export function Categories() {
+  const { categories, isLoading } = useGetCategories()
+  const { selectedTag, setSelectedTag } = useFilter()
+
+  if (isLoading) {
+    return <LoadingTags />
+  }
 
   return (
-    <ul className="flex items-center gap-3">
-      <TagsList data={categories} />
-    </ul>
+    <section className="w-full">
+      <Swiper
+        tag="ul"
+        slidesPerView={'auto'}
+        spaceBetween={12}
+      >
+        {categories?.map((category, idx) => {
+          const isSelected = category === selectedTag
+          const isFirstItem = idx === 0
+
+          return (
+            <SwiperSlide
+              key={idx}
+              tag="li"
+              content={category}
+              onClick={() => setSelectedTag(category)}
+              style={{ display: 'flex', justifyContent: 'center', width: 'fit-content', zIndex: 0 }}
+              className={`
+                  py-2 px-4 border border-solid border-purple100 rounded-full text-purple100 text-sm cursor-pointer hover:bg-purple200 hover:text-gray100 active:bg-purple200 active:text-gray100
+                  ${isSelected || (isFirstItem && selectedTag === null) ? `bg-purple200 text-white` : ''}
+              `}
+            >
+              {category}
+            </SwiperSlide>
+          )
+        })}
+      </Swiper>
+    </section>
   )
 }
