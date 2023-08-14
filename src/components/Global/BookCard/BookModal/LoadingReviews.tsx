ff76e14e5a38c4520cb8f2543aa0963296d0cb23
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-08-14 07:38:10 -0300

    style: update to responsive

--- src/components/Global/BookCard/BookModal/LoadingReviews.tsx
+++ src/components/Global/BookCard/BookModal/LoadingReviews.tsx
@@ -8,8 +8,8 @@ export function LoadingReviews({ amountReviews }: LoadingReviewsProps) {
   return (
     <div className="space-y-3">
       {fakeList.map((_, idx) => (
-        <div key={idx} className="animate-pulse w-[33.75rem] h-[9.625rem] p-6 space-y-5 bg-gray700 rounded-lg">
-          <div className="flex justify-between">
+        <div key={idx} className="animate-pulse w-[17rem] h-64 xs:w-[22rem] xs:h-48 sm:w-[33.75rem] sm:h-[9.625rem] p-6 space-y-5 bg-gray700 rounded-lg">
+          <div className="flex flex-col gap-4 xs:flex-row xs:gap-0 xs:justify-between">
             <div className="flex gap-4">
               <div
                 role="img"
@@ -24,10 +24,10 @@ export function LoadingReviews({ amountReviews }: LoadingReviewsProps) {
             <span
               role="img"
               aria-label="ícones de estrelas"
-              className="w-24 h-4 bg-gray600 rounded"
+              className="order-first xs:order-none w-24 h-4 bg-gray600 rounded"
             />
           </div>
-          <p className="w-[30rem] h-12 bg-gray600 rounded" />
+          <p className="w-56 h-28 xs:w-[19rem] xs:h-20 sm:w-[30rem] sm:h-12 bg-gray600 rounded" />
         </div>
       ))}
     </div>
