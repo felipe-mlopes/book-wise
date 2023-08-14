743ff9d350bdb898c87e4a829d44494edcf9cb5c
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-08-14 07:45:50 -0300

    feat: change props name

--- src/components/Global/StarRating.tsx
+++ src/components/Global/StarRating.tsx
@@ -1,18 +1,19 @@
 import { Star } from 'lucide-react'
+import { ComponentProps } from 'react'
 
-interface StarProps {
-  fill: number
+interface StarProps extends ComponentProps<'div'> {
+  amount: number
 }
 
-export function StarRating({ fill }: StarProps) {
+export function StarRating({ amount, ...props }: StarProps) {
   return (
-    <div className='flex gap-1'>
+    <div className='flex gap-1' {...props}>
       {[...Array(5)].map((_, idx) => {
         idx += 1
         return (
-          <Star 
+          <Star
             key={idx}
-            className={`${idx <= fill && 'fill-purple100'} w-4 h-4 stroke-purple100`} 
+            className={`${idx <= amount && 'fill-purple100'} w-4 h-4 stroke-purple100`}
           />
         )
       })}
