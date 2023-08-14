24b216c48b17817c191338780bf95e203053f1c9
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-08-14 07:39:22 -0300

    refactor: change styles + add BookReviews comp.

--- src/components/Global/BookCard/index.tsx
+++ src/components/Global/BookCard/index.tsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 import { StarRating } from '@/components/Global/StarRating';
 import { BookModal } from './BookModal';
 import { BookInfo } from './BookModal/BookInfo';
+import { BookReviews } from './BookModal/BookReviews';
 
 interface BookCardTypes {
   id: string,
@@ -41,9 +42,9 @@ export function BookCard({
 
 
   return (
-    <div className={`space-y-5 px-5 py-4 rounded-lg bg-gray700 border-solid border-2 border-transparent hover:border-solid hover:border-2 hover:border-gray600 ${isTrending === 'trending' && `lg:min-w-[15rem] xl:min-w-[20.25rem]`}`}>
+    <div className={`space-y-5 px-5 py-4 w-64 h-40 md:w-80 md:h-auto rounded-lg bg-gray700 border-solid border-2 border-transparent hover:border-solid hover:border-2 hover:border-gray600 ${isTrending === 'trending' && `lg:min-w-[15rem] xl:min-w-[20.25rem]`}`}>
       <div
-        className="flex gap-5 cursor-pointer"
+        className="flex gap-3 md:gap-5 cursor-pointer"
         onClick={handleBookModal}
       >
         {isTrending === undefined && <Image
@@ -51,7 +52,7 @@ export function BookCard({
           alt="capa do livro"
           width={108}
           height={152}
-          className="rounded"
+          className="max-w-[5.0625rem] max-h-[7.125rem] md:max-w-[6.75rem] md:max-h-[9.5rem] rounded"
         />}
         {isTrending === 'trending' && <Image
           src={cover}
@@ -62,16 +63,16 @@ export function BookCard({
         />}
         <div className="flex flex-col justify-between">
           <div>
-            <strong className="text-gray100 text-base">
+            <strong className="text-gray100 text-base line-clamp-3">
               {name}
             </strong>
             <p className="text-gray400 text-sm">{author}</p>
           </div>
-          <StarRating fill={ratingsAverage} />
+          <StarRating amount={ratingsAverage} />
         </div>
       </div>
       <BookModal
-        bookId={id}
+        key={id}
         isOpen={isBookModalOpen}
         onClose={handleBookModal}
       >
@@ -84,6 +85,7 @@ export function BookCard({
           bookRatingsAmount={ratingsAmount}
           bookRatingsAverage={ratingsAverage}
         />
+        <BookReviews bookId={id} />
       </BookModal>
     </div>
 
