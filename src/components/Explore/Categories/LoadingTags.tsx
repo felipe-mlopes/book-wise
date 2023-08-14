5bc86345752dc5b30c67d9287c7f71915858df6b
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-08-14 07:28:22 -0300

    refactor: remove ChevronIcon component

--- src/components/Explore/Categories/LoadingTags.tsx
+++ src/components/Explore/Categories/LoadingTags.tsx
@@ -1,18 +1,15 @@
-import { ChevronIcon } from "@/components/Icons/ChevronIcon";
-
-
 export function LoadingTags() {
-  const fakeList = [...Array(8)]
+  const fakeList = [...Array(10)]
 
   return (
-    <ul className="flex items-center gap-3">
-      <ChevronIcon type="left" isDisabled={true} isTag={true} />
-      {fakeList.map((_, idx) => (
-        <li key={idx} className="animate-pulse">
-          <div className="w-[5.5rem] h-8 bg-gray700 rounded-full" />
-        </li>
-      ))}
-      <ChevronIcon type="right" isDisabled={false} isTag={true} />
-    </ul>
+    <section className="w-full">
+      <ul className="flex gap-3 max-w-full overflow-x-hidden">
+        {fakeList.map((_, idx) => (
+          <li key={idx} className="animate-pulse">
+            <div className="w-[5.5rem] h-8 bg-gray700 rounded-full" />
+          </li>
+        ))}
+      </ul>
+    </section>
   )
 }
