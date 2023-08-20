9a0a2d9cfe70ffa01a4cb2022891171f42845a77
felipe-mlopes <felipe.mlopes03@gmail.com>
2023-08-20 18:19:46 -0300

    feat: add props and styles + change context

--- src/components/Feed/BookReviewSection.tsx
+++ src/components/Feed/BookReviewSection.tsx
@@ -1,19 +1,21 @@
 'use client'
 
-import { FeedContext } from "@/contexts/feed-context";
+import { SectionContext } from "@/contexts/section-context";
 import { ReactNode, useContext } from "react";
 
 interface BookReviewSectionProps {
-  children: ReactNode
+  children: ReactNode,
+  isProfile?: boolean
 }
 
-export function BookReviewSection({ children }: BookReviewSectionProps) {
-  const { selectedSection } = useContext(FeedContext)
+export function BookReviewSection({ children, isProfile }: BookReviewSectionProps) {
+  const { selectedProfileSection } = useContext(SectionContext)
 
   return (
     <div
-      className={`${selectedSection !== 'Avaliações' && `hidden invisible md:block md:visible`} 
-      space-y-5 md:space-y-10 max-md:min-w-[12rem]`}
+      data-page={isProfile}
+      className={`${selectedProfileSection !== 'Livros Avaliados' && `hidden invisible md:block md:visible`} 
+      space-y-5 md:space-y-10 max-md:min-w-[12rem] data-[page=true]:md:mr-8 data-[page=true]:lg:mr-16`}
     >
       {children}
     </div>
